2198249ae728f7e3ac802d458d97f81022e50944
KeBiemen <ke.van.biemen@hva.nl>
2025-12-11 12:35:29 +0100

    camel case (fixed #9)

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // 1: selecteer de button
 let likebutton = document.querySelector('button')
+let likeButton = document.querySelector('button')
  
 // 2: wacht op een klik
 likebutton.addEventListener('click', function() {
+likeButton.addEventListener('click', function() {
   // Even checken of dit werkt..
   
   // 3: maak de sectie geel, door een class toe te voegen aan die sectie..  
+  // 3: maak de de geselecteerde knop kloppen, door een class toe te voegen aan die sectie..  
   document.querySelector('.hart').classList.toggle('liked')
 })
